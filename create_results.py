ccb45793a1aeee06247d48dc6f1b3891908d89fc
lpossner <lpossner>
2024-07-31 15:54:08 +0200

    Many more changes

--- create_results.py
+++ create_results.py
@@ -1,6 +1,8 @@
 import os
+import json
 import numpy as np
-import torchvision
+from PIL import Image
+from torch.utils.data import Dataset
 import torchvision.transforms as transforms
 from model import model
 
@@ -13,13 +15,39 @@ np.random.seed(seed)
 coords = np.load("data/coords.npy")
 
 # run simulations with coords
-transform = transforms.Compose(
-    [transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))]
+class MyDataset(Dataset):
+
+    def __init__(self, path: str, transform=None, train: bool=True):
+        self.transform = transform
+        self.path = path
+        self.mode = 'train' if train else 'test'
+        
+        with open (f"{path}/{self.mode}/{self.mode}.json", "r") as f:
+            self.dataset_info = json.load(f)
+        
+        self.data = [(k, v) for k, v in self.dataset_info.items()]
+
+    def __len__(self):
+        return len(self.dataset_info)
+    
+    def __getitem__(self, idx):
+        image_path, label = self.data[idx]
+        image = Image.open(f"{self.path}/{self.mode}/{image_path}")
+        if self.transform:
+            image = self.transform(image)
+        return image, label
+    
+train_dataset = MyDataset(
+    path="./data/al5083",
+    transform=transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+    ]),
+    train=True
 )
-testset = torchvision.datasets.MNIST(
-    root="./data", train=False, download=True, transform=transform
-)
-img, label = testset[0]
+
+img, label = train_dataset[0]
+
 results = model(coords, img, label)[:, np.newaxis]
 
 # save results
